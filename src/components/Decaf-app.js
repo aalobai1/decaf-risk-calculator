59cbf5b86a97b81642fad754444a44015388f493
Ali Alobaidi <alialobaidi@Alis-MacBook-Pro-7.local>
2019-03-06 23:23:20 -0500

    Implemented automatic calculation

--- src/components/Decaf-app.js
+++ src/components/Decaf-app.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import RiskCalculator from './RiskCalculator'
 import ToggleButton from './ToggleButton'
+import decafScore from './Data'
+import RiskCalculator2 from './RiskCalc2'
 
 
 export default class DecafApp extends React.Component {
@@ -10,31 +11,47 @@ export default class DecafApp extends React.Component {
         chestXrayCons : 0,
         acidemia : 0,
         atrialFib : 0,
+        decafScore : 0,
+        risk : undefined,
+        hospitalMortality : undefined,
+        care : undefined,
     }
 
-    componentDidUpdate() {
+    componentDidUpdate(prevState) {
         console.log("updating")
     }
-   
 
+    calculateDecafScore = () => {
+        const num = (this.state.emrcd + this.state.eosinopenia + this.state.chestXrayCons + this.state.acidemia + this.state.atrialFib)
+        this.setState(() => (
+            {
+                hospitalMortality : decafScore[num].hospitalMortality,
+                risk : decafScore[num].risk,
+                care : decafScore[num].care,
+                decafScore : num
+         }
+        ))
+    }
 
     onSelectEmrcd = (option) => {
       if (option === "Not too dyspenic to leave house (eMRCD 1-4)") {
-        this.setState(() => ({emrcd : 0}))
+        this.setState({emrcd : 0}, () => {this.calculateDecafScore()})
       } else if (option === "Too dyspenic to leave house and wash/dress (eMRCD 5b)") {
-        this.setState(() => ({emrcd : 1}))
+        this.setState({emrcd : 1}, () => {this.calculateDecafScore()})
       } else if (option === "Too dyspenic to leave house but independent with washing/dressing (eMRCD 5a)") {
-        this.setState(() => ({emrcd : 2}))
+        this.setState({emrcd : 2}, () => {this.calculateDecafScore()})
       }
+      
     }
 
     onSelectYesorNo = (option, condition) => {
         if (option === "Yes") {
-            this.setState(() => ({ [condition] : 1 }))
+            this.setState({ [condition] : 1 }, () => {this.calculateDecafScore()})
         } else if (option === "No") {
-            this.setState(() => ({[condition] : 0}))
+            this.setState({ [condition] : 0 }, () => {this.calculateDecafScore()})
         }
     }
+
     render () {
         return (
             <div>
@@ -74,12 +91,12 @@ export default class DecafApp extends React.Component {
             condition={"acidemia"}
             onSelect={this.onSelectYesorNo}
             />
-            <RiskCalculator 
-            emrcd={this.state.emrcd}
-            eosinopenia={this.state.eosinopenia}
-            chestXrayCons={this.state.chestXrayCons}
-            acidemia={this.state.acidemia}
-            atrialFib={this.state.atrialFib}
+            <RiskCalculator2 
+            decafScore={this.state.decafScore}
+            risk={this.state.risk}
+            hospitalMortality={this.state.hospitalMortality}
+            care={this.state.hospitalMortality}
+            calculateDecafScore={this.calculateDecafScore}
             />
             </div>
         )
