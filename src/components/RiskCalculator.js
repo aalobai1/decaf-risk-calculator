5f8e0b48adb7445acd96c25bdf7146007c86a908
Ali Alobaidi <alialobaidi@Alis-MacBook-Pro-7.local>
2019-03-06 22:58:55 -0500

    more revision

--- src/components/RiskCalculator.js
+++ src/components/RiskCalculator.js
@@ -17,77 +17,18 @@ import decafScore from './Data'
 
         calculateDecafScore = () => {
             const num = (this.props.emrcd + this.props.eosinopenia + this.props.chestXrayCons + this.props.acidemia + this.props.atrialFib)
-            if (num === 0) {
-                this.setState(() => (
-                    {
-                        hospitalMortality : decafScore["0"].hospitalMortality,
-                        risk : decafScore["0"].risk,
-                        care : decafScore["0"].care,
-                        decafScore : num
-                 }
-                ))
-             } else if (num === 1) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["1"].hospitalMortality,
-                         risk : decafScore["1"].risk,
-                         care : decafScore["1"].care,
-                         decafScore : num
-                  }
-                 ))
-             } else if (num === 2) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["2"].hospitalMortality,
-                         risk : decafScore["2"].risk,
-                         care : decafScore["2"].care,
-                         decafScore : num
-                  }
-                 ))
-             } else if (num === 3) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["3"].hospitalMortality,
-                         risk : decafScore["3"].risk,
-                         care : decafScore["3"].care,
-                         decafScore : num
-                  }
-                 ))
-             } else if (num === 4) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["4"].hospitalMortality,
-                         risk : decafScore["4"].risk,
-                         care : decafScore["4"].care,
-                         decafScore : num
-                  }
-                 ))
-             } else if (num === 5) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["6"].hospitalMortality,
-                         risk : decafScore["6"].risk,
-                         care : decafScore["6"].care,
-                         decafScore : num
-                  }
-                 ))
-             } else if (num === 6) {
-                 this.setState(() => (
-                     {
-                         hospitalMortality : decafScore["6"].hospitalMortality,
-                         risk : decafScore["6"].risk,
-                         care : decafScore["6"].care,
-                         decafScore : num
-                  }
-                 ))
+            
+            this.setState(() => (
+                {
+                    hospitalMortality : decafScore[num].hospitalMortality,
+                    risk : decafScore[num].risk,
+                    care : decafScore[num].care,
+                    decafScore : num
              }
+            ))
         }
 
-        
     
-
-
-
         render = () => (
             <div>
             <h2>Decaf Score</h2>
